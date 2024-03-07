c0942fa8df1f08c7fb2c2155ede5787c3670c423
mandeep-mahra <mandeepmehara1@gmail.com>
2024-03-07 20:53:13 +0530

    fix : resolve path issue

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App(){
   return(
     <>
       <Routes>
-        <Route exact path="/coins/:coin" 
+        <Route path="/coins/:coin"
           element={<Home coinId={useParams().coin}/>}
         />
       </Routes>
